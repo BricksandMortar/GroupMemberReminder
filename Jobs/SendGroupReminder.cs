4219eec1af2cee2e0a1058b6364cdb76c89ed9f1
Arran France <arranf@gmail.com>
2016-10-12 17:01:29 +0100

    Add option to exclude Group Location's schedule

--- Jobs/SendGroupReminder.cs
+++ Jobs/SendGroupReminder.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using Quartz;
 using Quartz.Util;
 using Rock;
@@ -9,14 +8,14 @@ using Rock.Attribute;
 using Rock.Communication;
 using Rock.Data;
 using Rock.Model;
-using Rock.Web.Cache;
 
-namespace com.bricksandmortarstudio.ScheduleGroupReminder.Jobs
+namespace com.bricksandmortarstudio.ScheduledGroupReminder.Jobs
 {
     [GroupField( "Root Group", "The root group" )]
     [IntegerField( "Look Ahead", "The number of days ahead a schedule should be considered for emailing. NOTE: If this job runs multiple times during this interval group members will receive multiple emails" )]
     [SystemEmailField( "Email Template", "The system email that will be used as a template to send to the recipients" )]
     [BooleanField( "Save Communications", "Whether a communication record should be saved for emails sent via this job", Key = "IsSaved" )]
+    [BooleanField("Ignore Group Locations Schedules", "If selected group location schedules won't be included when calculating scheduled groups", true, key:"IgnoreGroupLocations")]
     public class SendGroupReminder : IJob
     {
         public void Execute( IJobExecutionContext context )
@@ -24,6 +23,7 @@ namespace com.bricksandmortarstudio.ScheduleGroupReminder.Jobs
             var dataMap = context.JobDetail.JobDataMap;
 
             bool isSaved = dataMap.GetBooleanValue( "IsSaved" );
+            bool ignoreGroupLocations = dataMap.GetBooleanValue( "IgnoreGroupLocations" );
             string rootGroupGuid = dataMap.GetString( "RootGroup" );
             string emailTemplateGuid = dataMap.GetString( "EmailTemplate" );
             string lookAhead = dataMap.GetString( "LookAhead" );
@@ -39,10 +39,24 @@ namespace com.bricksandmortarstudio.ScheduleGroupReminder.Jobs
             var rootGroup = groupService.Get( rootGroupGuid.AsGuid() );
 
 
-            // find child groups where the next scheduled date is within the cutoff period
-            var descendentGroups = rootGroup.Groups.Where( g => g.Schedule != null && g.Schedule.NextStartDateTime != null && g.Schedule.NextStartDateTime < cutOff || g.GroupLocations != null && g.GroupLocations.Any( gl => gl.Schedules.Any( s => s.NextStartDateTime < cutOff ) ) );
+            // Find child groups where the next scheduled date is within the cutoff period
+            IEnumerable<Group> descendentGroups; 
 
-            if ( ( descendentGroups.Any() ) )
+            if (!ignoreGroupLocations)
+            {
+                descendentGroups = rootGroup.Groups
+                                .Where( g => ( g.Schedule != null && g.Schedule.NextStartDateTime != null && g.Schedule.NextStartDateTime < cutOff ) || ( g.GroupLocations != null && g.GroupLocations.Any( gl => gl.Schedules.Any( s => s.NextStartDateTime < cutOff ) ) ) )
+                                .ToList();
+            }
+            else
+            {
+                descendentGroups = rootGroup.Groups
+                .Where( g =>  g.Schedule != null && g.Schedule.NextStartDateTime != null && g.Schedule.NextStartDateTime < cutOff  )
+                .ToList();
+            }
+            
+
+            if ( descendentGroups.Any() )
             {
                 int mailedCount = 0;
                 foreach ( var group in descendentGroups )
@@ -51,7 +65,8 @@ namespace com.bricksandmortarstudio.ScheduleGroupReminder.Jobs
                     var groupMembers = group.Members
                                             .Where( m =>
                                                  m.GroupMemberStatus == GroupMemberStatus.Active
-                                                 && !string.IsNullOrWhiteSpace( m.Person.Email));
+                                                 && !string.IsNullOrWhiteSpace( m.Person.Email))
+                                                 .ToList();
 
                     if ( groupMembers.Any() )
                     {
@@ -59,17 +74,19 @@ namespace com.bricksandmortarstudio.ScheduleGroupReminder.Jobs
                         var appRoot = Rock.Web.Cache.GlobalAttributesCache.Read().GetValue( "ExternalApplicationRoot" );
 
                         //Schedule to merge for lava. Pick the group schedule as a default and then the soonest nextstartdatetime otherwise
-                        Schedule selectedSchedule = group.Schedule ?? group.GroupLocations.OrderBy(gl => gl.Schedules.First().NextStartDateTime).FirstOrDefault( gl => gl.Schedules.Any() ).Schedules.First();
+                        var selectedSchedule = group.Schedule ?? group.GroupLocations.OrderBy(gl => gl.Schedules.FirstOrDefault().NextStartDateTime).FirstOrDefault( gl => gl.Schedules.Any() ).Schedules.FirstOrDefault();
 
                         foreach ( var groupMember in groupMembers )
                         {
-                            var mergeFields = new Dictionary<string, object>();
-                            mergeFields.Add( "Group", group );
-                            mergeFields.Add( "GroupMember", groupMember);
-                            mergeFields.Add( "Person", groupMember.Person );
-                            mergeFields.Add( "Schedule", selectedSchedule );
+                            var mergeFields = new Dictionary<string, object>
+                            {
+                                {"Group", group},
+                                {"GroupMember", groupMember},
+                                {"Person", groupMember.Person},
+                                {"Schedule", selectedSchedule}
+                            };
 
-                            List<string> recipients = new List<string>() { groupMember.Person.Email };
+                            var recipients = new List<string> { groupMember.Person.Email };
 
                             Email.Send( emailTemplate.From.ResolveMergeFields( mergeFields ), emailTemplate.FromName.ResolveMergeFields( mergeFields ), emailTemplate.Subject.ResolveMergeFields( mergeFields ), recipients, emailTemplate.Body.ResolveMergeFields( mergeFields ), appRoot, null, null, isSaved );
                             mailedCount++;
@@ -77,7 +94,7 @@ namespace com.bricksandmortarstudio.ScheduleGroupReminder.Jobs
                     }
                 }
 
-                context.Result = string.Format( "{0} group members were emailed", mailedCount );
+                context.Result = string.Format( "{0} group members were emailed in the following group".PluralizeIf( descendentGroups.Count() > 1) + string.Join(",", descendentGroups.AsEnumerable()), mailedCount );
             }
 
             else
